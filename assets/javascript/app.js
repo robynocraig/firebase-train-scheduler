f02823b1b4d204e298f7a7247978444e80811715
robynocraig <robynocraig@gmail.com>
2018-04-23 22:56:30 -0500

    added firebase JS link and firebase config values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,89 @@
-$(document).ready(function() {
+// variables
+// moment js function to know current time
+// click function to add train schedule
+// firebase function to store values
+// appending the firebase values to the table
 
+var config = {
+    apiKey: "AIzaSyCTRNWhUz_05Y1B44T4baTJmFJzD06e5ro",
+    authDomain: "train-scheduler-4aed7.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4aed7.firebaseio.com",
+    projectId: "train-scheduler-4aed7",
+    storageBucket: "train-scheduler-4aed7.appspot.com",
+    messagingSenderId: "98715473707"
+  };
+  
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+// 2. Button for adding Employees
+$("#add-employee-btn").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var empName = $("#employee-name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#start-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+
+  // Creates local "temporary" object for holding employee data
+  var newEmp = {
+    name: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newEmp);
+
+  // Logs everything to console
+  console.log(newEmp.name);
+  console.log(newEmp.role);
+  console.log(newEmp.start);
+  console.log(newEmp.rate);
+
+  // Alert
+  alert("Employee successfully added");
+
+  // Clears all of the text-boxes
+  $("#employee-name-input").val("");
+  $("#role-input").val("");
+  $("#start-input").val("");
+  $("#rate-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var empName = childSnapshot.val().name;
+  var empRole = childSnapshot.val().role;
+  var empStart = childSnapshot.val().start;
+  var empRate = childSnapshot.val().rate;
+
+  // Employee Info
+  console.log(empName);
+  console.log(empRole);
+  console.log(empStart);
+  console.log(empRate);
+
+  // Prettify the employee start
+  var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+
+  // Calculate the months worked using hardcore math
+  // To calculate the months worked
+  var empMonths = moment().diff(moment(empStart, "X"), "months");
+  console.log(empMonths);
+
+  // Calculate the total billed rate
+  var empBilled = empMonths * empRate;
+  console.log(empBilled);
+
+  // Add each train's data into the table
+  $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+  empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
 });
